112852d54070ab7235f70265d4316b1f23206000
Emiliano Amador <61723324+EmilianoAmador@users.noreply.github.com>
2020-05-01 17:39:08 -0400

    Update QuickDBD-export (1).sql

--- Starter_Files/ERD/QuickDBD-export (1).sql
+++ Starter_Files/ERD/QuickDBD-export (1).sql
@@ -1,8 +1,33 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 12.2
+-- Dumped by pg_dump version 12.2
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--Drop Table if it Exist
+
 DROP TABLE IF EXISTS card_holder;
 DROP TABLE IF EXISTS credit_card;
 DROP TABLE IF EXISTS merchant_categories;
 DROP TABLE IF EXISTS merchant;
 DROP TABLE IF EXISTS transaction;
+
 
 CREATE TABLE card_holder (
 	id INT PRIMARY KEY NOT NULL,
